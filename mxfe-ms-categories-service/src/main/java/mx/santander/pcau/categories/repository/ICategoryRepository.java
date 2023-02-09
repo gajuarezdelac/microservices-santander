d23dffb88b29d8dd8e0bb5adcc5f930c87998c27
Gabriel Juarez de la cruz <gjuarez@agoconsultores.com.mx>
2023-02-09 17:58:57 -0600

    AP

---
+++ mxfe-ms-categories-service/src/main/java/mx/santander/pcau/categories/repository/ICategoryRepository.java
@@ -0,0 +1,5 @@
+package mx.santander.pcau.categories.repository;
+
+public interface ICategoryRepository {
+
+}
---
+++ mxfe-ms-config-service/mxfe-ms-config-service/src/test/java/mx/santander/pcau/configserver/MxfeMsConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.santander.pcau.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxfeMsConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
